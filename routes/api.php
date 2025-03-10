f4586abed675e34191e419d1c0ba203c59a167ea
ahmed-abdelh3dy <ahmedmohhamed236@gmail.com>
2025-03-10 02:39:49 +0200

    modify the route for user

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/user/register', [AuthController::class, 'register']);
+Route::post('/user/login', [AuthController::class, 'login']);
