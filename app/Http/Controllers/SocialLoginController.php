be81e7c3afab3ae96577ec208c319561a868c8e1
ahmed-abdelh3dy <ahmedmohhamed236@gmail.com>
2025-03-13 10:44:40 +0200

    Enable login about google

---
+++ app/Http/Controllers/SocialLoginController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+use Throwable;
+use Illuminate\Support\Str;
+
+class SocialLoginController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+
+    public function callback($provider)
+    {
+
+
+        try {
+            $provider_user = Socialite::driver($provider)->user();
+            $user = User::where([
+                'provider' => $provider,
+                'provider_id' => $provider_user->id
+            ])->first();
+            
+
+
+            if (!$user) {
+                $user = User::create([
+                    'provider' => $provider,
+                    'provider_id' => $provider_user->id,
+                    'name' => $provider_user->name,
+                    'email' => $provider_user->email,
+                    'password' => Hash::make(Str::random(8)),
+                    'provider_token' => $provider_user->token,
+
+                ]);
+            }
+
+
+            Auth::login($user);
+            return 'login done';
+
+            // return redirect()->route('home');
+
+
+        } catch (Throwable $e) {
+            return redirect()->route('user.login')->withErrors([
+                'email' => $e->getMessage(),
+            ]);
+        }
+    }
+}
+
+
