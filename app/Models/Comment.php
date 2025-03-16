5daedbd7343269fd64c71ff15c7760c5b7165e82
eithar wageh <eitharwageh@gmail.com>
2025-03-16 15:35:01 +0200

    add comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+use HasFactory;
+protected $fillable = ['post_id','user_id','comment'];
+public function post(){
+    return $this->belongsTo(Post::class);
+}
+public function user(){
+    return $this->belongsTo(User::class);
+}
 }
