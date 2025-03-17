f456e4172f64b270bbe39b5e401c71c2d2939d58
ahmed332 <ahmedmonsef787@gmail.com>
2025-03-17 14:44:04 +0200

    add order route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +21,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/user/logout', [AuthController::class, 'logout']);
 });
 
-
+Route::get('/orders', [OrderController::class, 'index']);       
+Route::post('/orders', [OrderController::class, 'store']);     
+Route::get('/orders/{id}', [OrderController::class, 'show']);  
+Route::put('/orders/{id}', [OrderController::class, 'update']); 
+Route::delete('/orders/{id}', [OrderController::class, 'destroy']); 
 
 Route::post('/user/register', [AuthController::class, 'register']);
 Route::post('/user/login', [AuthController::class, 'login']);
