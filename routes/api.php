47accaed62b36a1427053c73e7d898334faffeac
ahmed332 <ahmedmonsef787@gmail.com>
2025-03-17 16:21:40 +0200

    order all

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/user/logout', [AuthController::class, 'logout']);
 });
 
-Route::get('/orders', [OrderController::class, 'index']);       
-Route::post('/orders', [OrderController::class, 'store']);     
-Route::get('/orders/{id}', [OrderController::class, 'show']);  
-Route::put('/orders/{id}', [OrderController::class, 'update']); 
-Route::delete('/orders/{id}', [OrderController::class, 'destroy']); 
+
+Route::apiResource('orders', controller: OrderController::class);
+
+// Route::get('/orders', [OrderController::class, 'index']);       
+// Route::post('/orders', [OrderController::class, 'store']);     
+// Route::get('/orders/{id}', [OrderController::class, 'show']);  
+// Route::put('/orders/{id}', [OrderController::class, 'update']); 
+// Route::delete('/orders/{id}', [OrderController::class, 'destroy']); 
 
 Route::post('/user/register', [AuthController::class, 'register']);
 Route::post('/user/login', [AuthController::class, 'login']);
