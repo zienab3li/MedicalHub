4b219f826d1acf05e3ffefd313d04b163a8ba654
ahmed-abdelh3dy <ahmedmohhamed236@gmail.com>
2025-04-11 13:42:23 +0200

    fix social login

--- app/Http/Controllers/SocialLoginController.php
+++ app/Http/Controllers/SocialLoginController.php
@@ -18,44 +18,83 @@ class SocialLoginController extends Controller
     }
 
 
-    public function callback($provider)
-    {
+    // public function callback($provider)
+    // {
 
 
-        try {
-            $provider_user = Socialite::driver($provider)->user();
-            $user = User::where([
-                'provider' => $provider,
-                'provider_id' => $provider_user->id
-            ])->first();
+    //     try {
+    //         $provider_user = Socialite::driver($provider)->user();
+    //         $user = User::where([
+    //             'provider' => $provider,
+    //             'provider_id' => $provider_user->id
+    //         ])->first();
             
 
 
-            if (!$user) {
-                $user = User::create([
-                    'provider' => $provider,
-                    'provider_id' => $provider_user->id,
-                    'name' => $provider_user->name,
-                    'email' => $provider_user->email,
-                    'password' => Hash::make(Str::random(8)),
-                    'provider_token' => $provider_user->token,
+    //         if (!$user) {
+    //             $user = User::create([
+    //                 'provider' => $provider,
+    //                 'provider_id' => $provider_user->id,
+    //                 'name' => $provider_user->name,
+    //                 'email' => $provider_user->email,
+    //                 'password' => Hash::make(Str::random(8)),
+    //                 'provider_token' => $provider_user->token,
 
-                ]);
-            }
-
-
-            Auth::login($user);
-            return 'login done';
-
-            // return redirect()->route('home');
+    //             ]);
+    //         }
 
 
-        } catch (Throwable $e) {
-            return redirect()->route('user.login')->withErrors([
-                'email' => $e->getMessage(),
+    //         Auth::login($user);
+    //         // return 'login done';
+
+    //         // return redirect()->route('home');
+
+
+    //     } catch (Throwable $e) {
+    //         return redirect()->route('user.login')->withErrors([
+    //             'email' => $e->getMessage(),
+    //         ]);
+    //     }
+    // }
+
+    public function callback($provider)
+{
+    try {
+        $provider_user = Socialite::driver($provider)->user();
+        $user = User::where([
+            'provider' => $provider,
+            'provider_id' => $provider_user->id
+        ])->first();
+
+        if (!$user) {
+            $user = User::create([
+                'provider' => $provider,
+                'provider_id' => $provider_user->id,
+                'name' => $provider_user->name,
+                'email' => $provider_user->email,
+                'password' => Hash::make(Str::random(8)),
+                'provider_token' => $provider_user->token,
             ]);
         }
+
+        Auth::login($user);
+        
+        $token = $user->createToken('auth-token')->plainTextToken;
+        
+        $userData = [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'role' => $user->role,
+            'token' => $token
+        ];
+        
+        return redirect()->away('http://localhost:4200?token=' . $token . '&user=' . urlencode(json_encode($userData)));
+
+    } catch (Throwable $e) {
+        return redirect()->away('http://localhost:4200/login?error=' . urlencode($e->getMessage()));
     }
+}
 }
 
 
